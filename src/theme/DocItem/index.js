d74702b8cb48e96682deb3b0df16331757564fb4
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-11-29 18:49:39 -0800

    maybe causing compile error?

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import OriginalDocItem from "@theme-original/DocItem";
-import Comment from "../../components/Comments"
+import Comment from "../../components/comments"
 
 export default function CustomDocItem(props) {
 	return (
